c9e3b3bd7d33b986ede84990391c2ede8a54f8a5
fronzen100826 <cmgg919@gmail.com>
2022-11-22 20:12:17 -0500

    first commit

---
+++ hello_timeout.js
@@ -0,0 +1,22 @@
+// let myTimeout = setTimeout(myGreeting, 3000);
+
+// function myGreeting(){
+//   console.log("4311o, th3r3, w0r1d");
+// }
+
+
+let myTimeout = setTimeout(myGreeting, 1000);
+let myTimeout1 = setTimeout(myGreeting1, 2000);
+let myTimeout2 = setTimeout(myGreeting2, 3000);
+
+function myGreeting() {
+  console.log("4311o");
+}
+
+function myGreeting1() {
+  console.log("th3r3");
+}
+
+function myGreeting2() {
+  console.log("w0r1d");
+}
